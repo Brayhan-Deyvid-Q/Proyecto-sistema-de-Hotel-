195357cdd0dd278cb8850ffcd4011a68683bee94
Jose <jvilcanquic@unjbg.edu.pe>
2024-07-28 19:09:36 -0500

    Se agregaron estructuras en huespedes.h

--- huespedes.h
+++ huespedes.h
@@ -1,6 +1,18 @@
 #ifndef HUESPEDES_H
 #define HUESPEDES_H
 
+struct Habitaciones {
+    int num_habi;
+    char tipo[20];
+};
+
+struct Huesped {
+    char Nombre[30];
+    char Telefono[10];
+    char dni[10];
+    struct Habitaciones hab;
+};
+
 void huespedesHotel();
 	
 #endif
